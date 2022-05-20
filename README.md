a665b50a9edc7a1fea6f208db57649eac6bd8462
Rodrigo Melo <nalajejazz@gmail.com>
2022-05-20 00:22:01 -0300

    Formatando Frontend

--- README.md
+++ README.md
@@ -27,4 +27,10 @@ npm run e2e
 npm test
 ```
 
+##awesome
+https://github.com/vuejs/awesome-vue
+
+##templates
+http://vuejs-templates.github.io/webpack/
+
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div id="app">
-    <img  src="./assets/logo.png">
+  <div   id="app">
+    <nav class="navbar navbar-light bg-dark mb-3">
+      <div class="container-fluid">
+      <a class="lk" href="">
+<span class="navbar-brand mb-0 h1 text-light">Inicio</span>
+</a>
+</div>
+    </nav>
     <router-view/>
   </div>
 </template>
@@ -12,12 +18,19 @@ export default {
 </script>
 
 <style>
+body{
+  background-color: #2c3e50;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
+  background-color: #2c3e50;
+}
+.lk{
+  text-decoration: none;
 }
 </style>
