22cfe740892c541e24da34610473c9831b0baf43
Rodrigo Melo <nalajejazz@gmail.com>
2022-05-20 19:18:24 -0300

    v-for e v-bind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div  class="hello"> <div class="container text-dark card  ">
+  <div  class="hello"> <div class="container text-dark card mt-4 ">
   <div class="row align-items-start ">
     <div class="col">
       <img width="100%" :src="img" alt="img" />
@@ -10,7 +10,15 @@
    <h3 class="text-start">Classe: {{classe}}</h3>
    <h4 v-if="msgCond" class="text-start text-info">Disponivel</h4>
    <h4 v-else-if="!msgCond" class="text-start text-danger">Indiponivel</h4>
+<div>
+  <h5 class="text-dark">Items</h5>
+  <ul class="list-group list-group-flush my-3 ">
+    <li class="text-dark list-group-item align-items-center" v-for="atr in atributos">
+    {{atr}}
+    </li>
+</ul>
 
+</div>
     </div>
   </div>
 </div>
@@ -20,6 +28,7 @@
 </template>
 
 <script>
+/* eslint-disable no-new */
 let bl= true
 let img=['https://i.imgur.com/ANhvnbE.gif',
 'https://i.pinimg.com/originals/91/05/3a/91053ac18b8b3368d29a1409ad6be5f1.gif']
@@ -32,10 +41,12 @@ export default {
       img: img[0],
       classe: classe[0],
       msgCond: bl,
-      xp: 10 
+      xp: 10,
+      atributos: ['Cajado','Capa Mágica','Adaga']
     }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
