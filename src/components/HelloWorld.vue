2edbbdd0eebb50c4bb0b9a93b1e7ec1ad83e8015
Rodrigo Melo <nalajejazz@gmail.com>
2022-05-20 22:58:33 -0300

    Correções eslint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,12 +8,12 @@
 
    <h3 class="text-start"> Xp:{{xp}}</h3>
    <h3 class="text-start">Classe: {{classe}}</h3>
-   <h4 v-if="msgCond" class="text-start text-info">Disponivel</h4>
-   <h4 v-else-if="!msgCond" class="text-start text-danger">Indiponivel</h4>
+   <h5 v-if="msgCond" class="text-start text-info">Disponivel</h5>
+   <h5 v-else-if="!msgCond" class="text-start text-danger">Indiponivel</h5>
 <div>
   <h5 class="text-dark">Items</h5>
   <ul class="list-group list-group-flush my-3 ">
-    <li class="text-dark list-group-item align-items-center" v-for="atr in atributos">
+    <li class="text-dark list-group-item align-items-center" v-for="atr in atributos" :key="atr.id">
     {{atr}}
     </li>
 </ul>
@@ -28,11 +28,9 @@
 </template>
 
 <script>
-/* eslint-disable no-new */
-let bl= true
-let img=['https://i.imgur.com/ANhvnbE.gif',
-'https://i.pinimg.com/originals/91/05/3a/91053ac18b8b3368d29a1409ad6be5f1.gif']
-let classe=['Arcano','Bardo']
+
+let img = ['https://i.imgur.com/ANhvnbE.gif', 'https://i.pinimg.com/originals/91/05/3a/91053ac18b8b3368d29a1409ad6be5f1.gif']
+let classe = ['Arcano', 'Bardo']
 
 export default {
   name: 'HelloWorld',
@@ -40,9 +38,9 @@ export default {
     return {
       img: img[0],
       classe: classe[0],
-      msgCond: bl,
+      msgCond: false,
       xp: 10,
-      atributos: ['Cajado','Capa Mágica','Adaga']
+      atributos: ['Cajado', 'Capa Mágica', 'Adaga']
     }
   }
 }
